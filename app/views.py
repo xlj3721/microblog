a411417222c95b4f303df0ea310ca739e5c9aa6b
Tarbo <dcreekp@gmail.com>
2014-08-12 12:05:52 +1000

    receiving form data; LoginForm() flash and validate_on_submit functions in views.py, {% with messages %} and {% if messages %} in base.html

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+
 def index():
     user = {'nickname': 'Miguel'} # fake user
     posts = [ # fake list of posts
@@ -26,11 +27,22 @@ def index():
         user = user,
         posts = posts)
 
+
 @app.route('/login/', methods = ['GET', 'POST']) 
 # methods tells Flask that this view function accepts GET and POST requests
 # default is just GET
+
 def login():
     form = LoginForm()
+    
+    if form.validate_on_submit():
+        # validate_on_submit function will be False if form is empty, if form is filled
+        # and valid (True) Flask will gather the data, invalid fill (False)
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' +
+        str(form.remember_me.data))
+        # flash function is a quick way to show a message
+        return redirect('/index')
+        
     return render_template('login.html',
         title = 'Sign In',
         form = form)
