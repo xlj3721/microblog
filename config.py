3b6749575443d3a244cd836e6a0a971a89955a79
Tarbo <dcreekp@gmail.com>
2014-08-12 22:10:08 +1000

    setting up SQLAlchemy on config.py and __init__.py

--- config.py
+++ config.py
@@ -7,3 +7,11 @@ OPENID_PROVIDERS = [
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLACHEMY_DATABASE+URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLACHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+                                             
