4ac10e73ea4a24be73d609dbb8cb8fb384705fc6
Tarbo <dcreekp@gmail.com>
2014-08-12 18:35:30 +1000

    more on the login from; openids on config.py, views.py and login.html with javscript

--- app/views.py
+++ app/views.py
@@ -45,4 +45,5 @@ def login():
         
     return render_template('login.html',
         title = 'Sign In',
-        form = form)
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
--- config.py
+++ config.py
@@ -1,2 +1,9 @@
 CSRF_ENABLED = True # prevents cross-site request forgery 
 SECRET_KEY = 'you-will-never-guess'# creates a cryptographic token used to validate a form
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
