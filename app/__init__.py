2f27096c1a27eddb60210868d9873460fb7fe90d
Tarbo <dcreekp@gmail.com>
2014-08-14 10:44:07 +1000

    configuring __init__.py for Flask-Login and Flask-OpenID extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object('config')
-db = SQLAlchemy(app)
+app = Flask(__name__) # creates a flask web application
+app.config.from_object('config') # tells flask to configure app with config.py
+db = SQLAlchemy(app)	# creates an SQLAlchemy instance of app for a database
 
 from app import views, models
+# tells flask to import views.py and models.py from app the folder
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+oid =OpenID(app, os.path.join(basedir,'tmp')) # tmp folder for OpenID to store files
