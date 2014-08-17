752b6980e140e7a866cb5d8488c59ac5c1b1fb2f
Tarbo <dcreekp@gmail.com>
2014-08-17 12:57:16 +1000

    __init__.py correction to import everything first

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,6 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__) # creates a flask web application
-app.config.from_object('config') # tells flask to configure app with config.py
-db = SQLAlchemy(app)	# creates an SQLAlchemy instance of app for a database
-
 from app import views, models
 # tells flask to import views.py and models.py from app the folder
 
@@ -13,6 +9,13 @@ from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
 
-lm = LoginManager()
-lm.init_app(app)
-oid =OpenID(app, os.path.join(basedir,'tmp')) # tmp folder for OpenID to store files
+app = Flask(__name__) # creates a flask web application
+app.config.from_object('config') # tells flask to configure app with config.py
+
+db = SQLAlchemy(app)	# creates an SQLAlchemy instance of app for a database
+
+lm = LoginManager() # creates an object that lets app and Flask_Login work together
+lm.init_app(app) # configures the Flask_Login application object to app
+lm.login_view = 'login' # informs lm what the login view is 
+
+oid = OpenID(app, os.path.join(basedir,'tmp')) # tmp folder for OpenID to store files
