536494ee1b1c3dceb1c3ab0ea5930e2b73cc39e6
Tarbo <dcreekp@gmail.com>
2014-08-23 11:12:37 +1000

    VI a new from and new template so users can edit their profile; forms.py and edit.html

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,11 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, BooleanField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired, Length # used to be Required but is deprecated, now called DataRequired
 
 class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
+    openid = TextField('openid', validators = [DataRequired()])
     remember_me = BooleanField('remember_me', default = False)
+
+class EditForm(Form):
+    nickname = TextField('nickname', validators = [DataRequired()])
+    about_me = TextField('about_me', validators = [Length(min= 0, max = 140)])
